9bb012960a7d5e64c75aeae51efa02c7a92891bf
Juho Päivärinta <juho.paivarinta@outlook.com>
2025-09-06 17:49:09 +0300

    feat(src/courses): Init CS.100 course env

--- src/courses/cs.100/main.py
+++ src/courses/cs.100/main.py
@@ -1,6 +1,17 @@
 def main() -> None:
-    print("Test")
+    """Use this function for your course work tasks.
+
+    Returns
+    -------
+    None
+        The function should return no value
+    """
+    greeting: str = "World!"
+    print(f"Hello {greeting}")
 
 
+# This helps control code execution by preventing unintended executions
+# The code is only run when the file is executed as a script
+# Not when it is imported as a module
 if __name__ == "__main__":
     main()
